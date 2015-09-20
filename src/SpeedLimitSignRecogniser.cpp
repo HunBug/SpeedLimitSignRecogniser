eca73fd539461c6f2193ce8f0f31af68f4eae1af
HunBug <akos.strack+github@gmail.com>
2015-09-20 13:56:52 +0200

    +setup boost (replace absolute paths)!

--- src/SpeedLimitSignRecogniser.cpp
+++ src/SpeedLimitSignRecogniser.cpp
@@ -7,11 +7,26 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include <boost/foreach.hpp>
+#include <boost/filesystem.hpp>
 using namespace std;
 
-int main() {
+//TODO: replace absolute paths in the eclipse project configurations
+
+int main(int argc, char* argv[]) {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	int i;
-	cin >> i;
+	std::string s;
+	 int a[10];
+	 for(int i=0;i<10;i++)
+	 {
+	 a[i]=i+10;
+	 }
+	cin >> s;
+	 BOOST_FOREACH(int k, a)
+	 {
+		 cout<<"\t"<<k;
+	 }
+	 std::cout << argv[0] << " " << boost::filesystem::file_size(argv[0]) << '\n';
 	return 0;
 }
